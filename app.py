83f97130dbd8e30f3770bb25c37eaf29b14758af
Abhishek Harsh <abhishekharsh3411@gmail.com>
2023-05-11 16:26:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from sklearn.preprocessing import MinMaxScaler
 app = Flask(__name__)
 
 model32 = pickle.load(open('parkinson_model32.pkl', 'rb'))
-@app.route('/form', methods=['GET','POST'])
-def home_page():
+@app.route('/', methods=['GET','POST'])
+def Home():
     return render_template('index.html')
 
 
