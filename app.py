98636b70a6d7666c2e5ad3e40a6254c6019e7175
Abhishek Harsh <abhishekharsh3411@gmail.com>
2023-05-17 13:59:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def predict_parkinson():
 
     result = model32.predict(values)
 
-    if(result[0]):
+    if(result):
         result = 'Positive Report' 
     else:
         result = 'Negative Report'
